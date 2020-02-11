b6930b2939bff4358a0c1875c2cf46f24b142bc8
Sabrina Trachsler <sabrina.trachsler@hotmail.com>
2020-02-11 15:51:50 +0100

    updated controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,45 @@
 class ApplicationController < ActionController::Base
+  # As a user, I can list tasks
+  def index
+    @tasks = Tasks.all
+  end
+
+  # As a user, I can view the details of a task
+  def show
+    @task = Tasks.find(params[:id])
+  end
+
+  # As a user, I can add a new task
+  def new
+    @tasks = Task.new
+  end
+
+  def create
+    Task.create(task_params)
+    redirect_to tasks_path
+  end
+
+  # As a user, I can edit a task (mark as completed / update title & details)
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @taks.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  # As a user, I can remove a task
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # As a user, I can list tasks
+  get "tasks", to: "tasks#index"
+  # As a user, I can view the details of a task
+  get "tasks/:id", to: "tasks#show"
+  # As a user, I can add a new task
+  get "tasks/new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
+  # As a user, I can edit a task (mark as completed / update title & details)
+  get "tasks/:id/edit", to: "tasks#edit"
+  patch "tasks/:id", to: "tasks#update"
+  # As a user, I can remove a task
+  delete "tasks/:id", to: "tasks#destroy"
 end
